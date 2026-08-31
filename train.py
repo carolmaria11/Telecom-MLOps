0f1832d406765a62f42dd3fceca7b7dc4f5fd3fe
Carol Anderia Maria <130856984+carolmaria11@users.noreply.github.com>
2026-08-31 06:04:46 +0530

    Change RandomForestClassifier n_estimators to 100

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ y = df['Failure_Within_48Hrs']
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=42)
 
-model= RandomForestClassifier(n_estimators=150,random_state=42)
+model= RandomForestClassifier(n_estimators=100,random_state=42)
 model.fit(X_train,y_train)
 
 acc = model.score(X_test,y_test)
